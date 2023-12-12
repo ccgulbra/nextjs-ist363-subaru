d0a8c13c6f465737e3c46b51bf38fbbcd527f5aa
ccgulbra <92338979+ccgulbrandsen@users.noreply.github.com>
2023-12-12 18:23:00 -0500

    attendance 12/12

--- pages/vehicles/index.js
+++ pages/vehicles/index.js
@@ -2,8 +2,6 @@ import Container from '../../components/Container';
 import FilterBar from '../../components/FilterBar';
 import Grid from '../../components/Grid';
 import Layout from '../../components/Layout';
-import Link from 'next/link';
-import Image from 'next/image';
 import { useState } from 'react';
 
 import { getAllVehicles, getVehicleTypes } from '../../lib/api';
@@ -51,26 +49,9 @@ const VehiclesPage = ({ vehicles, vehicleTypes }) => {
                 activeItem={activeVehicleType}
                 setActiveItem={setActiveVehicleType}
             />
-            <Grid>
-                {filteredVehicles.map((vehicle, index) => {
-                    const { title, slug, vehicleInformation } = vehicle.node; 
-                    const { trimLevels } = vehicleInformation; 
-                    return <article key={index}>
-                        {trimLevels && trimLevels[0].images.thumbnail &&
-                            <Image 
-                                src={trimLevels[0].images.thumbnail.node.sourceUrl}
-                                alt={trimLevels[0].images.thumbnail.node.altText}
-                                width={trimLevels[0].images.thumbnail.node.mediaDetails.width}
-                                height={trimLevels[0].images.thumbnail.node.mediaDetails.height}
-                            />
-                        }
-                        <h3>{title}</h3>
-                        <p>
-                            <Link href={`/vehicles/${slug}`}>learn more</Link>
-                        </p>
-                    </article>
-                })}
-            </Grid>
+            <Grid 
+                items={filteredVehicles}
+            />
         </Container>
     </Layout>
 }
